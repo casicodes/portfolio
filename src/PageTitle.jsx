f1961fc7ea57d61b0e54a998f8f4157d01c65927
Abhishek KC <sendtokcabhi@gmail.com>
2024-08-23 17:53:06 +1000

    Update desc

--- src/PageTitle.jsx
+++ src/PageTitle.jsx
@@ -1,8 +1,8 @@
 function PageTitle() {
   return (
     <div className="max-w-xl">
-      <h1 className="text-4xl font-bold tracking-tight mb-2">Motion Craft</h1>
-      <p className="text-gray-500 text-lg font-light">
+      <h1 className="text-4xl font-bold tracking-tight mb-4">Motion Craft</h1>
+      <p className="text-gray-500 text-lg">
         I'm{" "}
         <a
           href="https://abhishekkc.framer.website/"
