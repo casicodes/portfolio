b5495f627489e1d2fb946fd6f00e207226a1d57a
Abhishek KC <sendtokcabhi@gmail.com>
2024-09-06 14:46:28 +1000

    Add text-wrap to description

--- src/PageTitle.jsx
+++ src/PageTitle.jsx
@@ -1,13 +1,13 @@
 function PageTitle() {
   return (
-    <div className="max-w-xl">
+    <div>
       <h1 className="text-4xl font-bold tracking-tight mb-4">Motion Craft</h1>
-      <p className="text-gray-500 text-lg">
+      <p className="text-gray-500 text-lg text-wrap">
         I'm{" "}
         {/* <a href="https://abhishekkc.framer.website/" className="underline">
           Abhishek
         </a> */}
-        Abhishek , motion craft is my playground where I create
+        Abhishek, motion craft is my playground where I create
         micro-interactions using react, tailwind & framer motion.
       </p>
     </div>
