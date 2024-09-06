e8d63cd654705ef5b92348045ab753ee961c8ee8
Abhishek KC <sendtokcabhi@gmail.com>
2024-09-06 14:43:44 +1000

    Remove link

--- src/PageTitle.jsx
+++ src/PageTitle.jsx
@@ -4,11 +4,11 @@ function PageTitle() {
       <h1 className="text-4xl font-bold tracking-tight mb-4">Motion Craft</h1>
       <p className="text-gray-500 text-lg">
         I'm{" "}
-        <a href="https://abhishekkc.framer.website/" className="underline">
+        {/* <a href="https://abhishekkc.framer.website/" className="underline">
           Abhishek
-        </a>
-        , motion craft is my playground where I create micro-interactions using
-        react, tailwind & framer motion.
+        </a> */}
+        Abhishek , motion craft is my playground where I create
+        micro-interactions using react, tailwind & framer motion.
       </p>
     </div>
   );
