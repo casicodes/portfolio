c69dc68ca477ce9f54c1c2ac3020878e1a7d992f
Abhishek KC <sendtokcabhi@gmail.com>
2024-09-06 14:48:37 +1000

    Add text-wrap:balance

--- src/PageTitle.jsx
+++ src/PageTitle.jsx
@@ -2,7 +2,7 @@ function PageTitle() {
   return (
     <div>
       <h1 className="text-4xl font-bold tracking-tight mb-4">Motion Craft</h1>
-      <p className="text-gray-500 text-lg text-wrap">
+      <p className="text-gray-500 text-lg text-balance">
         I'm{" "}
         {/* <a href="https://abhishekkc.framer.website/" className="underline">
           Abhishek
