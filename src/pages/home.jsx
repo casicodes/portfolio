872fbe27da96155c0f079f0975223d22f1677c1d
Abhishek KC <sendtokcabhi@gmail.com>
2024-12-18 23:55:48 +1100

    Upscale

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,16 +4,18 @@ function Home() {
     <header className="flex flex-col gap-y-[200px]">
       <div className="flex flex-col gap-y-16">
         <div className="flex flex-col gap-4">
-          <h1 className="text-gray-800 font-semibold text-lg">Abhishek KC</h1>
-          <p className="text-gray-500 leading-6">
+          <h1 className="text-gray-800 text-2xl text-balance font-semibold">
+            Abhishek KC
+          </h1>
+          <p className="text-gray-500 text-lg">
             Software designer focused on the intersection of design, UI
             development and micro-interactions to make products feel alive.
           </p>
-          <p className="text-gray-500 leading-6">
+          <p className="text-gray-500 text-lg">
             Currently at{" "}
             <a
               href="https://www.lyrebirdhealth.com/au"
-              className="text-gray-800"
+              className="text-gray-800 font-medium"
             >
               Lyrebird Health
             </a>
@@ -28,24 +30,24 @@ function Home() {
           </p>
           <div className="flex flex-col gap-10">
             <div className="flex flex-col gap-2">
-              <Link to="/motion" className="capitalize">
+              <Link to="/motion" className="text-lg capitalize font-medium">
                 Micro-interactions for web{" "}
                 <span className="text-gray-400">· 2024</span>{" "}
               </Link>
 
               <div className="flex flex-col gap-4">
-                <p className="text-gray-500">
+                <p className="text-lg text-gray-500">
                   Micro-interactions using react, tailwind & framer motion.
                 </p>
               </div>
             </div>
             <div className="flex flex-col gap-2">
-              <h3 className="capitalize">
+              <h3 className="text-lg capitalize font-medium">
                 AMR Hair & Beauty mobile app{" "}
                 <span className="text-gray-400">· 2023</span>{" "}
               </h3>
               <div className="flex flex-col gap-4">
-                <p className="text-gray-500">
+                <p className="text-lg text-gray-500">
                   Designed and shipped the first version of mobile app that led
                   to 161k first week sales, improved retention (+82.5%), reduced
                   abandonment rate (-11.5%) and service tickets (-50%).
@@ -53,12 +55,12 @@ function Home() {
               </div>
             </div>
             <div className="flex flex-col gap-2">
-              <h3 className="capitalize">
+              <h3 className="text-lg capitalize font-medium">
                 AMR Hair & Beauty e-commerce website{" "}
                 <span className="text-gray-400">· 2022</span>{" "}
               </h3>
               <div className="flex flex-col gap-4">
-                <p className="text-gray-500">
+                <p className="text-lg text-gray-500">
                   Helped migrate, redesign and develop e-commerce platform,
                   resulting in increase in sales (+200%), average order value
                   (+77%) and improved workflow for the team.
@@ -79,9 +81,22 @@ function Home() {
             {/* <Link to="/motion" className="text-gray-500 text-lg">
             Explore my motion library
           </Link> */}
-            <a href="mailto:sendtokcabhi@gmail.com">Email</a>
-            <a href="https://www.linkedin.com/in/abhishekkc/">Linkedin</a>
-            <a href="https://drive.google.com/file/d/1gql-u0tsQSIhNFqfIU1rWQK5E-LhAiHh/view">
+            <a
+              href="mailto:sendtokcabhi@gmail.com"
+              className="text-lg font-medium"
+            >
+              Email
+            </a>
+            <a
+              href="https://www.linkedin.com/in/abhishekkc/"
+              className="text-lg font-medium"
+            >
+              Linkedin
+            </a>
+            <a
+              href="https://drive.google.com/file/d/1gql-u0tsQSIhNFqfIU1rWQK5E-LhAiHh/view"
+              className="text-lg font-medium"
+            >
               Resume
             </a>
           </div>
