0c619cf5e02b7c39029c04604c86b847b5b76f81
Abhishek KC <sendtokcabhi@gmail.com>
2024-12-30 20:05:17 +1100

    Add scroll to top component

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import "./App.css";
 import Home from "./pages/home";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Motion from "./pages/motion";
+import ScrollToTop from "./ScrollToTop";
 
 function App() {
   return (
     <main className="px-4 py-24 max-w-[600px] mx-auto">
+      {/* <div className="noise-bg"></div> */}
       <Router>
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/motion" element={<Motion />} />
